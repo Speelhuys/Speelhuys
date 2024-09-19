fe5c4e9d5f785869c1e242a56e016dbf2003ebd0
Huib Minderhoud <huibminderhout@gmail.com>
2024-09-19 11:55:08 +0200

    update

--- admin/admin.php
+++ admin/admin.php
@@ -54,7 +54,7 @@ $sets = set::filterSets($id, $name, $brandid, $themeid, $age, $price);
     </style>
 </head>
 
-<body style="background-image: url(../images/brickwall.png); background-repeat:no-repeat; background-position: fixed; background-size: 100%;">
+<body style="background-image: url(../images/brickwall.png); background-position: fixed; background-size: 100%;">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
             <div class="col-1">
@@ -80,11 +80,7 @@ $sets = set::filterSets($id, $name, $brandid, $themeid, $age, $price);
         <h3 style="color: limegreen;">Admin</h3>
         <br />
         <div class="mb-3">
-            <div class="row">
-                <input type="text" name="searchBar" class="form-control" style="width: 50%;" placeholder="Plaats hier uw zoekopdracht">
-                <h3>OF</h3>
                 <form method="POST" action="setAdd.php"><button type="submit" class="btn btn-primary">Maak een nieuwe blog</button></form>
-            </div>
             <br />
         </div>
         <div class="row">
@@ -95,14 +91,14 @@ $sets = set::filterSets($id, $name, $brandid, $themeid, $age, $price);
                         <?php foreach ($sets as $set) : ?>
                             <div class="col-md-4 mb-4">
                                 <div class="card h-100">
-                                    <img src="../images/<?php echo $set->image; ?>" class="card-img-top" alt="<?php echo $set->name; ?>">
+                                    <img src="../images/sets/<?php echo $set->image; ?>" class="card-img-top" alt="<?php echo $set->name; ?>">
                                     <div class="card-body">
                                         <h5 class="card-title"><?php echo $set->name; ?></h5>
                                         <p class="card-text"><?php echo $set->description; ?></p>
-                                        <p><strong>Price: </strong>€<?php echo number_format($set->price, 2); ?></p>
-                                        <p><strong>Age: </strong><?php echo $set->age; ?>+</p>
-                                        <p><strong>Pieces: </strong><?php echo $set->pieces; ?></p>
-                                        <p><strong>Stock: </strong><?php echo $set->stock; ?></p>
+                                        <p><strong>Prijs: </strong>€<?php echo number_format($set->price, 2); ?></p>
+                                        <p><strong>Leeftijd: </strong><?php echo $set->age; ?>+</p>
+                                        <p><strong>Stukken: </strong><?php echo $set->pieces; ?></p>
+                                        <p><strong>Voorraad: </strong><?php echo $set->stock; ?></p>
                                         <br />
                                         <a href="setEdit.php?id= <?php echo $set->id; ?>">Edit</a>
                                         <?php
@@ -122,45 +118,48 @@ $sets = set::filterSets($id, $name, $brandid, $themeid, $age, $price);
         </div>
 
         <!-- Sidebar filter bar -->
-        <div class="filter-bar" style="height: 100%; background-image: url(../images/brick.png);">
-            <h3>Filters</h3>
-            <form action="admin.php" method="GET">
-                <div class="mb-3">
-                    <strong><label for="set_brand_id" class="form-label">Merk</label></strong>
-                    <select name="set_brand_id" class="form-control" style="color:darkblue">
-                        <option value="">Alle Merken</option>
-                        <?php
-                        foreach ($brands as $brand) {
-                            echo '<option value="' . $brand->id . '">' . $brand->name . '</option>';
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <strong><label for="set_theme_id" class="form-label">Thema</label></strong>
-                    <select name="set_theme_id" class="form-control" style="color:darkred">
-                        <option value="">Alle Themas</option>
-                        <?php
-
-                        foreach ($themes as $theme) {
-                            echo '<option value="' . $theme->id . '">' . $theme->name . '</option>';
-                        }
-                        ?>
-                    </select>
-                </div>
-                <strong><label for="set_age" class="form-label">Minimum Leeftijd</label></strong>
-                <div class="slidecontainer">
-                    <input type="range" name="set_age" min="1" max="8" value="1" class="slider" id="ageRange">
-                    <p style="text-align: center;"><span id="ageValue"></span></p>
-                </div>
-                <div class="mb-3">
-                    <strong><label for="set_price" class="form-label" style="background-color: transparent;">Max Price (€)</label></strong>
-                    <input type="number" step="0.01" name="set_price" class="form-control" placeholder="">
-                </div>
-                <br />
-                <button type="submit" class="btn btn-primary">Zoeken</button>
-            </form>
+        <div class="filter-bar" style="height: 100%; background-image: url(images/brick.png);">
+      <h3>Filters</h3>
+      <form action="admin.php" method="GET">
+        <div class="mb-3">
+          <input type="text" name="set_search" class="form-control" placeholder="Typ hier uw zoekopdracht in">
         </div>
+        <div class="mb-3">
+          <strong><label for="set_brand_id" class="form-label">Merk</label></strong>
+          <select name="set_brand_id" class="form-control" style="color:darkblue">
+            <option value="">Alle Merken</option>
+            <?php
+            foreach ($brands as $brand) {
+              echo '<option value="' . $brand->id . '">' . $brand->name . '</option>';
+            }
+            ?>
+          </select>
+        </div>
+        <div class="mb-3">
+          <strong><label for="set_theme_id" class="form-label">Thema</label></strong>
+          <select name="set_theme_id" class="form-control" style="color:darkred">
+            <option value="">Alle Themas</option>
+            <?php
+
+            foreach ($themes as $theme) {
+              echo '<option value="' . $theme->id . '">' . $theme->name . '</option>';
+            }
+            ?>
+          </select>
+        </div>
+        <strong><label for="set_age" class="form-label">Minimum Leeftijd</label></strong>
+        <div class="slidecontainer">
+          <input type="range" name="set_age" min="1" max="8" value="1" class="slider" id="ageRange">
+          <p style="text-align: center;"><span id="ageValue"></span></p>
+        </div>
+        <div class="mb-3">
+          <strong><label for="set_price" class="form-label" style="background-color: transparent;">Max Price (€)</label></strong>
+          <input type="number" step="0.01" name="set_price" class="form-control" placeholder="">
+        </div>
+        <br />
+        <button type="submit" class="btn btn-primary">Zoeken!</button>
+      </form>
+    </div>
 
     </div>
 
