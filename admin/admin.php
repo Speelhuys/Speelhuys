233ce9464754a487af7269da7d5b46af2503ce0f
Huib Minderhoud <huibminderhout@gmail.com>
2024-09-19 15:29:33 +0200

    update

--- admin/admin.php
+++ admin/admin.php
@@ -32,7 +32,7 @@ $sets = set::filterSets($id, $name, $brandid, $themeid, $age, $price);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Garage - Products</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <style>
         /* Custom styling to move the filter bar to the right side */
         .filter-bar {
